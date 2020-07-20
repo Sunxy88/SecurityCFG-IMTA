19fc4e5fd0e1db062acc407b1f4e1bd3fbfc1b02
Xi SONG <sungxisx@icloud.com>
2020-07-20 09:53:04 +0200

    Nothing

--- target/test-classes/control-flow/ForStatement/ForStatement.java
+++
@@ -1,7 +0,0 @@
-public class ForStatement {
-    public void forStatement() {
-        for (int i = 0; i < 100; i++)
-            System.out.println(i + " th iteration");
-        System.out.println("Test finished");
-    }
-}
